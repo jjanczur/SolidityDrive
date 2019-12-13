3ee97a0c37fac1edaba0c7d3a5b16f93604a9f6e
Jacek Janczura <janczura.jacek@gmail.com>
2019-12-13 14:44:40 +0100

    Enable reload of the files on account change

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
-import SolidityDrive from "./contracts/SolidityDrive.json";
-import getWeb3 from "./getWeb3";
+import SolidityDriveContract from "./contracts/SolidityDrive.json";
+import getWeb3 from "./utils/getWeb3";
 import { StyledDropZone } from "react-drop-zone";
 import FileIcon, { defaultStyles } from "react-file-icon";
 import "react-drop-zone/dist/styles.css";
 import "bootstrap/dist/css/bootstrap.css";
 import { Table } from "reactstrap";
-
+import fileReaderPullStream from "pull-file-reader";
+import ipfs from "./utils/ipfs";
+import Moment from "react-moment";
 import "./App.css";
 
 class App extends Component {
@@ -22,15 +24,22 @@ class App extends Component {
 
       // Get the contract instance.
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SolidityDrive.networks[networkId];
+      const deployedNetwork = SolidityDriveContract.networks[networkId];
       const instance = new web3.eth.Contract(
-        SolidityDrive.abi,
+        SolidityDriveContract.abi,
         deployedNetwork && deployedNetwork.address
       );
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
       this.setState({ web3, accounts, contract: instance }, this.getFiles);
+
+      // On update event - changed metamaska account - reload page ang get correct files
+      web3.currentProvider.publicConfigStore.on("update", async () => {
+        const changedAccounts = await web3.eth.getAccounts();
+        this.setState({ accounts: changedAccounts });
+        this.getFiles();
+      });
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -41,30 +50,50 @@ class App extends Component {
   };
 
   getFiles = async () => {
-    const { account, contract } = this.state;
-    let filesLength = await contract.methods
-      .getLength()
-      .call({ from: account[0] });
-    let files = [];
-    for (let i = 0; i < filesLength; i++) {
-      let file = await contract.method.getFile(i).call({ from: account[0] });
-      files.push(file);
+    try {
+      const { accounts, contract } = this.state;
+      let filesLength = await contract.methods
+        .getLength()
+        .call({ from: accounts[0] });
+      let files = [];
+      for (let i = 0; i < filesLength; i++) {
+        let file = await contract.methods
+          .getFile(i)
+          .call({ from: accounts[0] });
+        files.push(file);
+      }
+      this.setState({ solidityDrive: files });
+    } catch (error) {
+      console.log(error);
     }
-    this.setState({solidityDrive: files});
   };
 
-  onDrop = async () => {
-    //TODO
+  onDrop = async file => {
+    try {
+      const { contract, accounts } = this.state;
+      const stream = fileReaderPullStream(file);
+      const result = await ipfs.add(stream);
+      const timestamp = Math.round(+new Date() / 1000);
+      const type = file.name.substr(file.name.lastIndexOf(".") + 1);
+      let uploaded = await contract.methods
+        .add(result[0].hash, file.name, type, timestamp)
+        .send({ from: accounts[0], gas: 300000 });
+      console.log(uploaded);
+      this.getFiles();
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   render() {
+    const { solidityDrive } = this.state;
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
       <div className="App">
         <div className="container pt-3">
-          <StyledDropZone />
+          <StyledDropZone onDrop={this.onDrop} />
           <Table>
             <thead>
               <tr>
@@ -76,17 +105,29 @@ class App extends Component {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <th>
-                  <FileIcon
-                    size={30}
-                    extension="docx"
-                    {...defaultStyles.docs}
-                  />
-                </th>
-                <th className="text-left">File name.docx</th>
-                <th className="text-right">2019/03/10</th>
-              </tr>
+              {solidityDrive !== []
+                ? solidityDrive.map((item, key) => (
+                    <tr>
+                      <th>
+                        <FileIcon
+                          size={30}
+                          extension={item[2]}
+                          {...defaultStyles[item[2]]}
+                        />
+                      </th>
+                      <th className="text-left">
+                        <a href={"https://ipfs.io/ipfs/" + item[0]}>
+                          {item[1]}
+                        </a>
+                      </th>
+                      <th className="text-right">
+                        <Moment format="YYYY/MM/DD" unix>
+                          {item[3]}
+                        </Moment>
+                      </th>
+                    </tr>
+                  ))
+                : null}
             </tbody>
           </Table>
         </div>
